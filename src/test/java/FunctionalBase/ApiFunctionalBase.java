409b09cb651b17e0d239479c527a397bf1008b2f
lasantha <lasantha.caldra@pearson.com>
2022-04-12 02:21:54 +0530

    java doc

--- src/test/java/FunctionalBase/ApiFunctionalBase.java
+++ src/test/java/FunctionalBase/ApiFunctionalBase.java
@@ -5,13 +5,26 @@ import io.restassured.http.Header;
 import io.restassured.response.Response;
 
 public class ApiFunctionalBase {
-
+    /**
+     *
+     * @param url
+     * @param contentType
+     * @return
+     */
 
     protected Response doGetRequest(String url, ContentType contentType){
 
         return null;
     }
 
+    /**
+     *
+     * @param url
+     * @param contentType
+     * @param header
+     * @param body
+     * @return
+     */
     protected Response doPostRequest(String url , ContentType contentType,Header header, String body){
         return  null;
     }
