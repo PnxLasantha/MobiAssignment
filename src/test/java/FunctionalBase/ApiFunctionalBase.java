8ed731ece610ff5a35171231a831268fb402bd33
lasantha <lasantha.caldra@pearson.com>
2022-04-12 02:04:31 +0530

    ApifunctioalBase- add content type

--- src/test/java/FunctionalBase/ApiFunctionalBase.java
+++ src/test/java/FunctionalBase/ApiFunctionalBase.java
@@ -1,17 +1,18 @@
 package FunctionalBase;
 
+import io.restassured.http.ContentType;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
 
 public class ApiFunctionalBase {
 
 
-    protected Response doGetRequest(String url){
+    protected Response doGetRequest(String url, ContentType contentType){
 
         return null;
     }
 
-    protected Response doPostRequest(String url , Header header, String body){
+    protected Response doPostRequest(String url , ContentType contentType,Header header, String body){
         return  null;
     }
 }
