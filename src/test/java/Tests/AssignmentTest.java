fe55057dddec329c45198cb12daaf350e1ef59c7
lasantha <lasantha.caldra@pearson.com>
2022-04-13 17:51:07 +0530

    Adding allure reports

--- src/test/java/Tests/AssignmentTest.java
+++ src/test/java/Tests/AssignmentTest.java
@@ -2,6 +2,9 @@ package Tests;
 
 import FunctionalBase.ApiFunctionalBase;
 import base.ApiBase;
+import io.qameta.allure.Allure;
+import io.qameta.allure.AllureLifecycle;
+import io.qameta.allure.Description;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
@@ -19,38 +22,51 @@ public class AssignmentTest extends ApiBase {
     Integer userID;
     JsonPath jpath;
     List<Integer> postList;
+    AllureLifecycle lifecycle = Allure.getLifecycle();
+
 
     @Test
-    public void getUserData() {
+    @Description("Get User ID")
+    public void getUserDataTest() {
+        //setting up different name for allure report
+        lifecycle.updateTestCase(testResult -> testResult.setName("Get User ID"));
+
         url = baseUrl + "/users";
         res = apiBase.doGetRequest(url);
         jpath = res.jsonPath();
+
+        //fetching user id of delphine
         userID = jpath.param("userName", userName).get("find{it.username == userName}.id");
         Assert.assertEquals(res.getStatusCode(), 200);
         Assert.assertNotNull(userID, userName + " not found");
 
     }
 
-    @Test(dependsOnMethods = {"getUserData"})
-    public void getUserPosts() {
+    @Test(dependsOnMethods = {"getUserDataTest"}, testName = "Get user posts by user id")
+    @Description("Get all the post from thee user Delp")
+    public void getPostsCreatedByUserTest() {
+        lifecycle.updateTestCase(testResult -> testResult.setName("Get user posts by user id"));
+
         url = baseUrl + "/posts";
         res = apiBase.doGetRequest(url);
-
         jpath = res.jsonPath();
         postList = jpath.param("userID", userID).get("findAll{it.userId == userID}.id");
-
         Assert.assertNotNull(postList);
 
     }
 
-    @Test(dependsOnMethods = {"getUserPosts"})
-    public void getComments() {
+    @Test(dependsOnMethods = {"getPostsCreatedByUserTest"})
+    @Description("Validate email address")
+    public void getPostedCommentsAndValidateEmailTest() {
+        lifecycle.updateTestCase(testResult -> testResult.setName("Validate comments and email"));
+
         url = baseUrl + "/comments";
         res = apiBase.doGetRequest(url);
         jpath = res.jsonPath();
         for (int s : postList) {
             List<String> email = jpath.param("post", s).get("findAll{it.postId == post}.email");
             for (String mail : email) {
+                //using apache common validator for validate emails
                 Assert.assertTrue(EmailValidator.getInstance().isValid(mail));
             }
 
