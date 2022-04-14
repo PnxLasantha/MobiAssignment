14ecb953973f489ce2cc0b0b7c55189eceeae798
lasantha <lasantha.caldra@pearson.com>
2022-04-14 14:41:08 +0530

    Add more test cases and README.md

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # MobiAssignment [![CircleCI](https://circleci.com/gh/PnxLasantha/MobiAssignment/tree/master.svg?style=svg&circle-token=1d3b561ce32b809871b71e2f15211b8e284a8047)](https://circleci.com/gh/PnxLasantha/MobiAssignment/tree/master)
-Mobi Assignment
+
+MobiQuity Assignment
+
+## Configuring Allure Reports
+
+This is not necessery to run the automation.This section is only need to generate and view ther allure reports
+
+### Windows
+
+1.Open a powershell window and install scoop using the following command `iwr -useb get.scoop.sh | iex` ( This will
+install scoop package manager for windows)
+
+2.Install allure using `scoop install allure`
+
+3.Verify allure installation `allure --version`
+
+Note - For mac please refer this document to configure
+
+## Running the Tests
+
+This is a Maven project. The tests can be executed from the command line using `mvn clean test`
+
+Execute the following command in the terminal to generate the
+reports `allure serve <location of the allure-results folder>` (This allure-results folder is created after executing
+the tests)
+
+## Tools used
+
+- Rest Assured - Version 5.0
+- TestNG - Testing framework Version 7.4
+- Allure - Reporting plugin Version 2.17
+- Java - Version 17.0.2
+- Json - Validate Jason schema Version 20220320
+- Commons Validator - Validate emails Version 1.7
+
+## Sample Test result
+
--- src/test/java/Tests/AssignmentTest.java
+++ src/test/java/Tests/AssignmentTest.java
@@ -5,23 +5,22 @@ import base.ApiBase;
 import io.qameta.allure.Allure;
 import io.qameta.allure.AllureLifecycle;
 import io.qameta.allure.Description;
-import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.apache.commons.validator.routines.EmailValidator;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AssignmentTest extends ApiBase {
     ApiFunctionalBase apiBase = new ApiFunctionalBase();
-    ContentType contentType;
     Response res;
     String url;
     Integer userID;
     JsonPath jpath;
-    List<Integer> postList;
+    List<Integer> resultList, photoID;
     AllureLifecycle lifecycle = Allure.getLifecycle();
 
 
@@ -50,21 +49,23 @@ public class AssignmentTest extends ApiBase {
         url = baseUrl + "/posts";
         res = apiBase.doGetRequest(url);
         jpath = res.jsonPath();
-        postList = jpath.param("userID", userID).get("findAll{it.userId == userID}.id");
-        Assert.assertNotNull(postList);
+        resultList = jpath.param("userID", userID).get("findAll{it.userId == userID}.id");
+        Assert.assertFalse(resultList.isEmpty());
 
     }
 
     @Test(dependsOnMethods = {"getPostsCreatedByUserTest"})
     @Description("Validate email address")
     public void getPostedCommentsAndValidateEmailTest() {
+        List<String> email;
         lifecycle.updateTestCase(testResult -> testResult.setName("Validate comments and email"));
 
         url = baseUrl + "/comments";
         res = apiBase.doGetRequest(url);
+        Assert.assertEquals(res.getStatusCode(), 200);
         jpath = res.jsonPath();
-        for (int s : postList) {
-            List<String> email = jpath.param("post", s).get("findAll{it.postId == post}.email");
+        for (int s : resultList) {
+            email = jpath.param("post", s).get("findAll{it.postId == post}.email");
             for (String mail : email) {
                 //using apache common validator for validate emails
                 Assert.assertTrue(EmailValidator.getInstance().isValid(mail));
@@ -73,6 +74,54 @@ public class AssignmentTest extends ApiBase {
         }
 
     }
+
+    @Test(description = "Verify user created photo albums", dependsOnMethods = {"getUserDataTest"})
+    public void getPhotoAlbumDetailsTest() {
+
+        lifecycle.updateTestCase(testResult -> testResult.setName("Verify user created photo albums"));
+
+        url = baseUrl + "/albums?userId=" + userID;
+        res = apiBase.doGetRequest(url);
+        jpath = res.jsonPath();
+        Assert.assertEquals(res.getStatusCode(), 200);
+        resultList = jpath.get("id");
+        Assert.assertFalse(resultList.isEmpty());
+
+
+    }
+
+    @Test(description = "Verify albums contains atleast one photo", dependsOnMethods = {"getPhotoAlbumDetailsTest"})
+    public void getUseUploadedPhotos() {
+        lifecycle.updateTestCase(testResult -> testResult.setName("Verify albums contains atleast one photo"));
+
+        url = baseUrl + "/photos";
+        res = apiBase.doGetRequest(url);
+        jpath = res.jsonPath();
+        Assert.assertEquals(res.getStatusCode(), 200);
+        for (int albumID : resultList) {
+            photoID = jpath.param("albumID", albumID).get("findAll{it.albumId == albumID}.id");
+
+            Assert.assertFalse(photoID.isEmpty());
+        }
+
+
+    }
+
+    @Test(description = "Verfiy Broken  urls", dependsOnMethods = {"getUseUploadedPhotos",})
+    public void verifyBrokenThumbnailLinks() {
+        lifecycle.updateTestCase(testResult -> testResult.setName("Verify broken urls "));
+        List<String> imageUrl = new ArrayList<>();
+        url = baseUrl + "/photos";
+        res = apiBase.doGetRequest(url);
+        jpath = res.jsonPath();
+        System.out.println(photoID);
+        for (int picID : photoID) {
+            imageUrl.add(jpath.param("picID", picID).getString("find{it.id == picID}.url"));
+        }
+        imageUrl.parallelStream().forEach(e -> Assert.assertEquals(apiBase.doGetRequest(e).getStatusCode(), 200));
+
+    }
+
 
     //validate for title and body
 
